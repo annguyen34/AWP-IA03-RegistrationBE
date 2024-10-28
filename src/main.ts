97ceb56211335710f643f6e8097a864572ffcdeb
annguyen34 <21120508@student.hcmus.edu.vn>
2024-10-29 00:39:45 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: /.+/ });
+  app.use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://frabjous-eclair-d699af.netlify.app/',
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
